0f1f768d3cff588522143889fc1ed77f3f05cf32
이재경 <jkl02138@gmail.com>
2024-03-22 09:44:23 +0900

    섹션 0. Node JS 회원가입 기능 ~ 토큰 생성까지

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-
 const config = require("./config/key");
-
+const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -42,6 +41,7 @@ app.post('/register', async (req, res) => {
     // 회원가입에 필요한 정보들을 client에서 가져오면 
     // 그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body);
+
     const userInfo = await user.save();
     res.status(200).json({ success: true });
   } catch (err) {
@@ -49,5 +49,36 @@ app.post('/register', async (req, res) => {
     res.status(500).json({ success: false, error: err.message });
   }
 });
+
+app.post('/login', async (req, res) => {
+  
+  try {
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    const user = await User.findOne({email: req.body.email});
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    
+    const isMatch = await user.comparePassword(req.body.password);
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    if(!isMatch) {
+      return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+    }
+    
+    // 비밀번호까지 같다면 token을 생성하기.
+    const token = await user.generateToken();
+    // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+    res.cookie("x_auth", user.token)
+    .status(200)
+    .json({loginSuccess: true, userId: user._id})
+      
+  } catch(err) {
+    console.error('Error occurred during user login: ', err);
+    res.status(400).json({loginSuccess: false, error: err.message});
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
